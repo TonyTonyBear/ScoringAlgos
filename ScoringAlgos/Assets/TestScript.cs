53631a9aa7b293d1048bb6d731a7ca259013ff1b
Tony <ajbarrettx@gmail.com>
2018-02-01 15:42:02 -0500

    Fix bugs in testScript

--- ScoringAlgos/Assets/TestScript.cs
+++ ScoringAlgos/Assets/TestScript.cs
@@ -4,127 +4,120 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+	private void Start()
+	{
+		initList();
 
-	<!--
+		Debug.Log(namMember[lstMember[cmp1][head1]]);
+		Debug.Log(namMember[lstMember[cmp2][head2]]);
+	}
 
-var namMember = new Array(
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+			sortList(-1);
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+			sortList(1);
+	}
 
-"You Are In Love",
-"Wonderland",
-"New Romantics",
-"Welcome To New York",
-"Blank Space",
-"Style",
-"Out Of The Woods",
-"All You Had To Do Was Stay",
-"Shake It Off",
-"I Wish You Would",
-"Bad Blood",
-"Wildest Dreams",
-"How You Get The Girl",
-"This Love",
-"I Know Places",
-"Clean"
+	string[] namMember = new string[]
+	{
 
-
-
-);
-
-
+	"You Are In Love",
+	"Wonderland",
+	"New Romantics",
+	"Welcome To New York",
+	"Blank Space",
+	"Style",
+	"Out Of The Woods",
+	"All You Had To Do Was Stay",
+	"Shake It Off",
+	"I Wish You Would",
+	"Bad Blood",
+	"Wildest Dreams",
+	"How You Get The Girl",
+	"This Love",
+	"I Know Places",
+	"Clean"
+	};
 
 	//*********************************************************
 
 
 
-	var lstMember = new Array();
+	List<List<int>> lstMember = new List<List<int>>();
+	List<int> parent = new List<int>();
+	List<int> equal = new List<int>();
+	List<int> rec = new List<int>();
 
-	var parent = new Array();
+	int cmp1, cmp2;
+	int head1, head2;
+	int nrec;
 
-	var equal = new Array();
-
-	var rec = new Array();
-
-	var cmp1, cmp2;
-
-	var head1, head2;
-
-	var nrec;
-
-
-
-	var numQuestion;
-
-	var totalSize;
-
-	var finishSize;
-
-	var finishFlag;
-
+	int numQuestion;
+	int totalSize;
+	int finishSize;
+	int finishFlag;
 
 
 	//The initialization of the variable+++++++++++++++++++++++++++++++++++++++++++++
 
-	function initList()
+	public void initList()
 	{
 
-		var n = 0;
-
-		var mid;
-
-		var i;
-
-
+		int n = 0;
+		int mid;
+		int i;
 
 		//The sequence that you should sort
 
-		lstMember[n] = new Array();
+		lstMember.Add(new List<int>()); //1.
 
-		for (i = 0; i < namMember.length; i++)
+		for (i = 0; i < namMember.Length; i++) //2.
 		{
 
-			lstMember[n][i] = i;
+			lstMember[n].Add(i);
 
 		}
 
-		parent[n] = -1;
+		parent.Add(-1); // 3.
 
-		totalSize = 0;
+		totalSize = 0; // 3.
 
-		n++;
+		n++; // 3.
 
 
 
-		for (i = 0; i < lstMember.length; i++)
+		for (i = 0; i < lstMember.Count; i++) // 4. - 
 		{
-
 			//And element divides it in two/more than two
 
 			//Increase divided sequence of last in first member
 
-			if (lstMember[i].length >= 2)
+			if (lstMember[i].Count >= 2) // 5.
 			{
 
-				mid = Math.ceil(lstMember[i].length / 2);
+				mid = (int)Mathf.Ceil(lstMember[i].Count / 2); // 6.
 
-				lstMember[n] = new Array();
+				lstMember.Add(new List<int>()); // 7.
 
-				lstMember[n] = lstMember[i].slice(0, mid);
+				lstMember[n] = lstMember[i].GetRange(0, mid); // 8.
 
-				totalSize += lstMember[n].length;
+				totalSize += lstMember[n].Count; // 9.
 
-				parent[n] = i;
+				parent.Add(i); // 10.
 
-				n++;
+				n++; // 10.
 
-				lstMember[n] = new Array();
+				lstMember.Add(new List<int>()); // 11.
 
-				lstMember[n] = lstMember[i].slice(mid, lstMember[i].length);
+				lstMember[n] = lstMember[i].GetRange(mid, (lstMember[i].Count) - mid); // 11.
 
-				totalSize += lstMember[n].length;
+				totalSize += lstMember[n].Count; // 12.
 
-				parent[n] = i;
+				parent.Add(i); // 13.
 
-				n++;
+				n++; // 13.
 
 			}
 
@@ -134,10 +127,10 @@ var namMember = new Array(
 
 		//Preserve this sequence
 
-		for (i = 0; i < namMember.length; i++)
+		for (i = 0; i < namMember.Length; i++)
 		{
 
-			rec[i] = 0;
+			rec.Add(0);
 
 		}
 
@@ -148,21 +141,18 @@ var namMember = new Array(
 		//List that keeps your results
 
 		//Value of link initial
-
-		// Value of link initial
-
-		for (i = 0; i <= namMember.length; i++)
+		for (i = 0; i <= namMember.Length; i++)
 		{
 
-			equal[i] = -1;
+			equal.Add(-1);
 
 		}
 
 
 
-		cmp1 = lstMember.length - 2;
+		cmp1 = lstMember.Count - 2;
 
-		cmp2 = lstMember.length - 1;
+		cmp2 = lstMember.Count - 1;
 
 		head1 = 0;
 
@@ -188,18 +178,18 @@ var namMember = new Array(
 
 	// 1&#65306;Chose the right
 
-	function sortList(flag)
+	public void sortList(int flag)
 	{
 
-		var i;
+		int i;
 
-		var str;
+		string str;
 
 
 
 		//rec preservation
 
-		if (flag < 0)
+		if (flag < 0) // "Left Choice"
 		{
 
 			rec[nrec] = lstMember[cmp1][head1];
@@ -225,7 +215,7 @@ var namMember = new Array(
 
 		}
 
-		else if (flag > 0)
+		else if (flag > 0) // "Right Choice"
 		{
 
 			rec[nrec] = lstMember[cmp2][head2];
@@ -251,7 +241,7 @@ var namMember = new Array(
 
 		}
 
-		else
+		else // "Center Choice - Can't Decide"
 		{
 
 			rec[nrec] = lstMember[cmp1][head1];
@@ -304,12 +294,12 @@ var namMember = new Array(
 
 		//Processing after finishing with one list
 
-		if (head1 < lstMember[cmp1].length && head2 == lstMember[cmp2].length)
+		if (head1 < lstMember[cmp1].Count && head2 == lstMember[cmp2].Count)
 		{
 
 			//List the remainder of cmp2 copies, list cmp1 copies when finished scanning
 
-			while (head1 < lstMember[cmp1].length)
+			while (head1 < lstMember[cmp1].Count)
 			{
 
 				rec[nrec] = lstMember[cmp1][head1];
@@ -324,12 +314,12 @@ var namMember = new Array(
 
 		}
 
-		else if (head1 == lstMember[cmp1].length && head2 < lstMember[cmp2].length)
+		else if (head1 == lstMember[cmp1].Count && head2 < lstMember[cmp2].Count)
 		{
 
 			//List the remainder of cmp1 copies, list cmp2 copies when finished scanning
 
-			while (head2 < lstMember[cmp2].length)
+			while (head2 < lstMember[cmp2].Count)
 			{
 
 				rec[nrec] = lstMember[cmp2][head2];
@@ -350,19 +340,18 @@ var namMember = new Array(
 
 		//Update a pro list
 
-		if (head1 == lstMember[cmp1].length && head2 == lstMember[cmp2].length)
+		if (head1 == lstMember[cmp1].Count && head2 == lstMember[cmp2].Count)
 		{
 
-			for (i = 0; i < lstMember[cmp1].length + lstMember[cmp2].length; i++)
+			for (i = 0; i < lstMember[cmp1].Count + lstMember[cmp2].Count; i++)
 			{
 
 				lstMember[parent[cmp1]][i] = rec[i];
 
 			}
 
-			lstMember.pop();
-
-			lstMember.pop();
+			lstMember.RemoveAt(lstMember.Count - 1);
+			lstMember.RemoveAt(lstMember.Count - 1);
 
 			cmp1 = cmp1 - 2;
 
@@ -379,7 +368,7 @@ var namMember = new Array(
 			if (head1 == 0 && head2 == 0)
 			{
 
-				for (i = 0; i < namMember.length; i++)
+				for (i = 0; i < namMember.Length; i++)
 				{
 
 					rec[i] = 0;
@@ -397,9 +386,9 @@ var namMember = new Array(
 		if (cmp1 < 0)
 		{
 
-			str = "Battle #" + (numQuestion - 1) + "<br>" + Math.floor(finishSize * 100 / totalSize) + "% sorted.";
+			str = "Battle #" + (numQuestion - 1) + "<br>" + Mathf.Floor(finishSize * 100 / totalSize) + "% sorted.";
 
-			document.getElementById("battleNumber").innerHTML = str;
+			//document.getElementById("battleNumber").innerHTML = str;
 
 
 
@@ -412,7 +401,7 @@ var namMember = new Array(
 		else
 		{
 
-			showImage();
+			//showImage();
 
 		}
 
@@ -426,31 +415,31 @@ var namMember = new Array(
 
 	//&#21517;&#21069;=Identification term
 
-	function showResult()
+	public void showResult()
 	{
 
-		var ranking = 1;
+		int ranking = 1;
 
-		var sameRank = 1;
+		int sameRank = 1;
 
-		var str = "";
+		string str = "";
 
-		var i;
+		int i;
 
 
 
 		str += "<table style=\"width:200px; font-size:18px; line-height:120%; margin-left:auto; margin-right:auto; border:1px solid #000; border-collapse:collapse\" align=\"center\">";
 
-		str += "<tr><td style=\"color:#ffffff; background-color:#000; text-align:center;\">Rank<\/td><td style=\"color:#ffffff; background-color:#000; text-align:center;\">Song<\/td><\/tr>";
+		//str += "<tr><td style=\"color:#ffffff; background-color:#000; text-align:center;\">Rank<\/td><td style=\"color:#ffffff; background-color:#000; text-align:center;\">Song<\/td><\/tr>";
 
 
 
-		for (i = 0; i < namMember.length; i++)
+		for (i = 0; i < namMember.Length; i++)
 		{
 
-			str += "<tr><td style=\"border:1px solid #000; text-align:center; padding-right:5px;\">" + ranking + "<\/td><td style=\"border:1px solid #000; padding-left:5px;\">" + namMember[lstMember[0][i]] + "<\/td><\/tr>";
+			//str += "<tr><td style=\"border:1px solid #000; text-align:center; padding-right:5px;\">" + ranking + "<\/td><td style=\"border:1px solid #000; padding-left:5px;\">" + namMember[lstMember[0][i]] + "<\/td><\/tr>";
 
-			if (i < namMember.length - 1)
+			if (i < namMember.Length - 1)
 			{
 
 				if (equal[lstMember[0][i]] == lstMember[0][i + 1])
@@ -472,11 +461,11 @@ var namMember = new Array(
 
 		}
 
-		str += "<\/table>";
+		//str += "<\/table>";
 
 
 
-		document.getElementById("resultField").innerHTML = str;
+		//document.getElementById("resultField").innerHTML = str;
 
 	}
 
