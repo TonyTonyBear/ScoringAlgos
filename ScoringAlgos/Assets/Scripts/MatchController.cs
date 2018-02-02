18350d6663a64dda03c285bbf66c4f7b8ec89481
Tony <ajbarrettx@gmail.com>
2018-02-02 11:23:42 -0500

    Add MatchController

--- ScoringAlgos/Assets/Scripts/MatchController.cs
+++ ScoringAlgos/Assets/Scripts/MatchController.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(DataProcessor))]
 public class MatchController : MonoBehaviour
 {
 	#region Properties
 
+	public KeyCode leftSelection, rightSelection;
 
 	#endregion
 
-	#region Monobehavior Functions
+	#region Monobehavior
+
+	private void Update()
+	{
+		if (!DataProcessor.instance.IsFinished)
+		{
+			if (Input.GetKeyDown(leftSelection))
+			{
+				SelectLeft();
+			}
+			if (Input.GetKeyDown(rightSelection))
+			{
+				SelectRight();
+			}
+		}
+	}
+
 	#endregion
 
-	#region Helper Functions
-	#endregion
+	#region Model Communication
 
+	private void SelectLeft()
+	{
+		DataProcessor.instance.SortList(-1);
+	}
+
+	private void SelectRight()
+	{
+		DataProcessor.instance.SortList(1);
+	}
+
+	#endregion
 }
